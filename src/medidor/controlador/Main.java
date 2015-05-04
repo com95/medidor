baa16b24a020ce2d28d1d5d8b3723ca771ad828c
Yuli <Yuli@Equipo>
2015-05-04 18:00:26 -0500

    Ya esta la BD conectada

--- src/medidor/controlador/Main.java
+++ src/medidor/controlador/Main.java
@@ -8,6 +8,11 @@ package medidor.controlador;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+import medidor.modelo.Modelo;
 import medidor.vista.UIMain;
 
 /**
@@ -23,11 +28,38 @@ public class Main {
         inicio = new CMain();
     }
     
-    public static void main(String args [])
+    public static void main(String args []) throws SQLException
     {
-        new Main();
+        Vector <Modelo> modelos = new Vector <Modelo> ();
+        
         Connection con = null;
         Statement stat = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/metricas","root", "123456");
+
+            // Creamos un Statement para poder hacer peticiones a la bd
+            stat = (Statement) con.createStatement();
+
+            // Seleccionar todos los datos ordenados por potencia
+            String seleccionar = "SELECT * FROM MODELOS";
+            ResultSet rs = stat.executeQuery(seleccionar);
+
+            // Recorrer todas las filas de la tabla Motor
+            while (rs.next()) {
+                Modelo m = new Modelo();
+                m.setCodigoModelo(Integer.parseInt(rs.getString("CodigoModelo")));
+                m.setNombreModelo(rs.getString("Nombre"));
+                
+                modelos.add(m);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {         
+            if (stat != null) stat.close();
+            if (con != null) con.close();    
+        }
         
+        new Main();
     }
 }
