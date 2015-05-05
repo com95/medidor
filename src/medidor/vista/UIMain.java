11d650b8698037c7b9af02462a2da624f17cbc1e
Yuli <Yuli@192.168.1.34>
2015-05-05 00:15:48 -0500

    Interfaz Grafica Principal Acabada

--- src/medidor/vista/UIMain.java
+++ src/medidor/vista/UIMain.java
@@ -21,7 +21,7 @@ public class UIMain extends javax.swing.JFrame {
     private IMain interfaceMain;
     
     public UIMain(IMain interfaceMain) {
-        this.getContentPane().setBackground(new java.awt.Color(229, 255, 204));
+        this.getContentPane().setBackground(new java.awt.Color(240, 240, 240));
         this.setTitle("MedA");
         this.setVisible(true);
         initComponents();
@@ -38,98 +38,142 @@ public class UIMain extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        btnOk = new javax.swing.JButton();
+        lblTitulo = new javax.swing.JLabel();
+        btnEvaluar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jList1 = new javax.swing.JList();
-        jLabel2 = new javax.swing.JLabel();
+        lstModelos = new javax.swing.JList();
+        lblEscoger = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        txtModelos = new javax.swing.JTextArea();
+        jSeparator1 = new javax.swing.JSeparator();
+        btnAyuda = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Medidor de Calidad");
         setIconImage(new javax.swing.ImageIcon(getClass().getResource("/recursos/icon.png")).getImage());
 
-        jLabel1.setFont(new java.awt.Font("Cambria", 1, 34)); // NOI18N
-        jLabel1.setText("MEDIDOR DE CALIDAD");
+        lblTitulo.setFont(new java.awt.Font("Cambria", 1, 32)); // NOI18N
+        lblTitulo.setText("MEDIDOR DE CALIDAD");
 
-        btnOk.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
-        btnOk.setForeground(new java.awt.Color(0, 102, 204));
-        btnOk.setIcon(new javax.swing.ImageIcon(getClass().getResource("/recursos/A2.png"))); // NOI18N
-        btnOk.setText("EVALUAR");
-        btnOk.setBorder(null);
-        btnOk.setBorderPainted(false);
-        btnOk.setContentAreaFilled(false);
-        btnOk.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        btnOk.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        btnOk.setIconTextGap(-3);
-        btnOk.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/recursos/A3.png"))); // NOI18N
-        btnOk.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/recursos/A1.png"))); // NOI18N
-        btnOk.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
-        btnOk.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        btnEvaluar.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        btnEvaluar.setForeground(new java.awt.Color(0, 51, 153));
+        btnEvaluar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/recursos/A2.png"))); // NOI18N
+        btnEvaluar.setText("EVALUAR");
+        btnEvaluar.setBorder(null);
+        btnEvaluar.setBorderPainted(false);
+        btnEvaluar.setContentAreaFilled(false);
+        btnEvaluar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnEvaluar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnEvaluar.setIconTextGap(-3);
+        btnEvaluar.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/recursos/A3.png"))); // NOI18N
+        btnEvaluar.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/recursos/A1.png"))); // NOI18N
+        btnEvaluar.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
+        btnEvaluar.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
 
-        jList1.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
-        jList1.setModel(new javax.swing.AbstractListModel() {
+        lstModelos.setFont(new java.awt.Font("Tahoma", 0, 15)); // NOI18N
+        lstModelos.setModel(new javax.swing.AbstractListModel() {
             String[] strings = { "Modelo ISO 9126", "Modelo McCall", "Modelo Peruano" };
             public int getSize() { return strings.length; }
             public Object getElementAt(int i) { return strings[i]; }
         });
-        jScrollPane1.setViewportView(jList1);
+        lstModelos.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lstModelosMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(lstModelos);
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 13)); // NOI18N
-        jLabel2.setText("Seleccione un estandar de calidad:");
+        lblEscoger.setFont(new java.awt.Font("Tahoma", 0, 13)); // NOI18N
+        lblEscoger.setText("Seleccione un estandar de calidad:");
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane2.setViewportView(jTextArea1);
+        txtModelos.setColumns(20);
+        txtModelos.setRows(5);
+        jScrollPane2.setViewportView(txtModelos);
+
+        btnAyuda.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        btnAyuda.setForeground(new java.awt.Color(0, 51, 153));
+        btnAyuda.setIcon(new javax.swing.ImageIcon(getClass().getResource("/recursos/help2.png"))); // NOI18N
+        btnAyuda.setText("Ayuda");
+        btnAyuda.setBorder(null);
+        btnAyuda.setBorderPainted(false);
+        btnAyuda.setContentAreaFilled(false);
+        btnAyuda.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnAyuda.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnAyuda.setIconTextGap(-3);
+        btnAyuda.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/recursos/help3.png"))); // NOI18N
+        btnAyuda.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/recursos/help1.png"))); // NOI18N
+        btnAyuda.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
+        btnAyuda.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(27, 27, 27)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel2)
-                            .addComponent(jScrollPane1)
-                            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 210, Short.MAX_VALUE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btnOk)))
-                .addContainerGap(28, Short.MAX_VALUE))
+                        .addGap(36, 36, 36)
+                        .addComponent(lblTitulo))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap(24, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(lblEscoger))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(18, 18, 18)
+                                        .addComponent(btnEvaluar))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(45, 45, 45)
+                                        .addComponent(btnAyuda))))
+                            .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 373, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(14, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel2)
-                .addGap(18, 18, 18)
+                .addComponent(lblTitulo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addComponent(lblEscoger)
+                        .addGap(9, 9, 9)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(btnOk))
-                .addContainerGap(24, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(btnEvaluar)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnAyuda)))
+                .addContainerGap(27, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void lstModelosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lstModelosMouseClicked
+        
+    }//GEN-LAST:event_lstModelosMouseClicked
+
     /**
      * @param args the command line arguments
      */
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnOk;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JList jList1;
+    private javax.swing.JButton btnAyuda;
+    private javax.swing.JButton btnEvaluar;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JLabel lblEscoger;
+    private javax.swing.JLabel lblTitulo;
+    private javax.swing.JList lstModelos;
+    private javax.swing.JTextArea txtModelos;
     // End of variables declaration//GEN-END:variables
 }
